f4a1546aa081bb4fda3962d4ecf47433f715806f
holabayor <liasu.olabayor@gmail.com>
2023-01-18 18:20:00 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 ''' Writing strings to Redis '''
 
 
@@ -20,3 +20,32 @@ class Cache():
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''
+            takes a key string argument and an optional Callable argument fn.
+            This callable will convert the data back to the desired format
+        '''
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str):
+        '''
+            Get the string value
+        '''
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str):
+        '''
+            Get and parameterize the integer values
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
